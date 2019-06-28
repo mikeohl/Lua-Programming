65d4b9ce31ab58e42eca046e9015853950ead1fe
Michael Ohl <mikeohl@live.com>
2019-06-27 21:25:09 -0700

    Test of Lua number conversion

---
+++ num_conversion_test.lua
@@ -0,0 +1,11 @@
+line = io.read()
+n = tonumber(line)
+if n == nil then
+  error(line .. " is not a valid number")
+else
+  print(n*2)
+end
+
+print(tostring(10) == "10")
+print(10 .. "" == "10")
+  
