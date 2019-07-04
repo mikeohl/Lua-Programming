77cb289b41cec5542c7ebcb2c03de1a3b21b3568
Michael Ohl <mikeohl@live.com>
2019-07-03 20:25:00 -0700

    do-while as repeat and numeric (traditional C) for loop

---
+++ control_structures.lua
@@ -0,0 +1,14 @@
+-- Control statements
+
+x = 0
+
+repeat
+  print(x)
+  x = x + 1
+until x == 10
+
+
+for i=1,10,4 do   -- step 4, so 1, 5, 9 to end
+  print "Hello"   -- no optional parentheses for a single 
+                  -- literal string
+end
