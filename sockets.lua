c8731d9b2c9e4b79ae1aa9b4edce361c5460055a
Michael Ohl <mikeohl@live.com>
2019-07-18 18:30:45 -0700

    receiving files through lua socket via coroutine

--- sockets.lua
+++ sockets.lua
@@ -1,11 +1,5 @@
 socket = require "socket"
 
-print(socket._VERSION)
-
----[[
-host = "www.w3.org"
-file = "/TR/REC-html32.html"
-
 
 function download (host, file)
   local c = assert(socket.connect(host, 80))
@@ -15,8 +9,10 @@ function download (host, file)
   
   while true do
     local s, status = receive(c)
-    count = count + string.len(s)
-    
+    if s then
+      count = count + string.len(s)
+    end
+
     if status == "closed" then break end
   end
   
@@ -24,10 +20,12 @@ function download (host, file)
   print(file, count)
 end
 
+
 function receive (connection)
-  connection:timeout(0)
+  connection:settimeout(0)
   
-  local s, status = connection:receive(2^10)
+  local s, status = connection:receive(2^10) -- get 1024 bytes
+  --print(s, status)
   
   if status == "timeout" then
     coroutine.yield(connection)
@@ -35,5 +33,57 @@ function receive (connection)
   
   return s, status
 end
+
+
+
+-- calls to get add coroutine to threads table
+function get (host, file)
+  local co = coroutine.create(function ()
+      download(host, file)
+    end)
+  table.insert(threads, co)
+end
+
+-- resume each thread and remove when they complete
+function dispatcher ()
+  while true do 
+    local n = #threads
+    print(n)
+    if n == 0 then break end -- quit when threads finish
+    
+    local connections = {}
+    
+    for i = 1, n do
+      local status, res = coroutine.resume(threads[i])
+      print(res)
+      if not res then
+        table.remove(threads, i)
+        break
+      else
+        table.insert(connections, res) 
+        -- insert thread to connections table
+      end
+    end
+    if #connections == n then
+      socket.select(connections)
+    end
+  end
+end
+
+print(socket._VERSION)
+
+---[[
+host = "www.w3.org"
+local file = "/TR/REC-html32.html"
+local file2 = "/TR/html401/html.txt"
+local file3 = "/TR/2002/REC-xhtml1-20020801/xhtml1.pdf"
+local file4 = "/TR/2000/REC-DOM-Level-2-Core-20001113/DOM2-Core.txt"
+
+threads = {}
+
+get(host, file); get(host, file2)
+get(host, file3); get(host, file4)
+
+dispatcher()
 
 --]]
